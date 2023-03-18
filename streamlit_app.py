a568a31910644bce774db6cd324c7c69b1931ade
smirs <saeed.mirshekari@gmail.com>
2023-03-18 17:10:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,12 +41,12 @@ ax.suptitle('Team Tempreture in the USA @' + today, fontsize=48)
 plt.xlabel(' ', fontsize=24)
 plt.ylabel(' ', fontsize=52)
 
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, , fontsize=24)
 
 timewin = 10
 for i in indict:
     sns.lineplot(x='timestamp', y='temperature_value', data=observations[i][observations[i]['timestamp'] >= (observations[i]['timestamp'][0] - pd.DateOffset(hours=timewin))], label= i)
-    plt.annotate(i, xy=(observations[i]['timestamp'][0], observations[i]['temperature_value'][0]))
+    plt.annotate(i, xy=(observations[i]['timestamp'][0], observations[i]['temperature_value'][0]), fontsize=24)
     
 xlabels = reversed(['Now','2 Hours Ago','4  Hours Ago','6  Hours Ago','8  Hours Ago','10 Hours Ago',
                     '12 Hours Ago','14 Hours Ago','16 Hours Ago','18 Hours Ago','20 Hours Ago'])
@@ -55,6 +55,6 @@ xlabels = [x for x in xlabels]
 locs, labels=plt.xticks()
 x_ticks = []
 # new_xticks= ['test' for d in locs]
-plt.xticks(locs, xlabels, rotation=90, horizontalalignment='right')
+plt.xticks(locs, xlabels, rotation=90, horizontalalignment='right', fontsize=24)
 
 st.pyplot(ax)
