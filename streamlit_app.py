bc5633195905cb2a8f9f98b824e3f981f3f57938
smirs <saeed.mirshekari@gmail.com>
2023-03-31 15:02:17 -0500

    sas

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,26 +78,27 @@ df = pd.DataFrame({'time':x, 'gain':gain, 'cost':cost})
 
 ax = sns.lineplot(x='time', y='gain', data=df, color = 'green', label = 'gain')
 ax = sns.lineplot(x='time', y='cost', data=df, color = 'red', label = 'cost')
-
-###########################################################
-import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
-###########################################################
-
-txt = st.text_area('Text to analyze', '''
-    It was the best of times, it was the worst of times, it was
-    the age of wisdom, it was the age of foolishness, it was
-    the epoch of belief, it was the epoch of incredulity, it
-    was the season of Light, it was the season of Darkness, it
-    was the spring of hope, it was the winter of despair, (...)
-    ''')
-
-price = st.slider('Listing Price?', 100000, 200000,)
-st.write("I'm ", price, 'years old', txt)
-
-#create your figure and get the figure object returned
-fig = plt.figure() 
-plt.plot([10000, 100000, 150000, 200000, price]) 
-
 st.pyplot(ax)
+
+###########################################################
+# import streamlit as st
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+###########################################################
+
+# txt = st.text_area('Text to analyze', '''
+#     It was the best of times, it was the worst of times, it was
+#     the age of wisdom, it was the age of foolishness, it was
+#     the epoch of belief, it was the epoch of incredulity, it
+#     was the season of Light, it was the season of Darkness, it
+#     was the spring of hope, it was the winter of despair, (...)
+#     ''')
+
+# price = st.slider('Listing Price?', 100000, 200000,)
+# st.write("I'm ", price, 'years old', txt)
+
+# #create your figure and get the figure object returned
+# fig = plt.figure() 
+# plt.plot([10000, 100000, 150000, 200000, price]) 
+
+
