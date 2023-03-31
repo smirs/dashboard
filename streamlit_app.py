94717939f73275ec582f9c77cc318a57013dc5c8
smirs <saeed.mirshekari@gmail.com>
2023-03-31 17:38:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 a = """Some text
 
-![Cool Image](https://d33wubrfki0l68.cloudfront.net/442bccd786f4d6c1b07f9f58aa3f894f4c38a0c2/e1e28/assets/img/olab_logo.png)
+![Cool Image](src="https://d33wubrfki0l68.cloudfront.net/442bccd786f4d6c1b07f9f58aa3f894f4c38a0c2/e1e28/assets/img/olab_logo.png" style="width:200px;")
 
 Some more text"""
 
