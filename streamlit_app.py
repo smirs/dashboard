a55bb38ae1866985ad3c1d001c1d37a1b09e7510
smirs <saeed.mirshekari@gmail.com>
2023-03-31 14:25:17 -0500

    sa

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,13 @@ st.pyplot(fig)
 ###########################################################
 
 import streamlit as st
+txt = st.text_area('Text to analyze', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+
 age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+st.write("I'm ", age, 'years old', txt)
