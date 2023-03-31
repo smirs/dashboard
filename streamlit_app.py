98d7621c10552da6e3f7de57c3fa9d9c6e81b130
smirs <saeed.mirshekari@gmail.com>
2023-03-31 17:51:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 a = """
 # Good Deal / Bad Deal
-![drawing](https://d33wubrfki0l68.cloudfront.net/442bccd786f4d6c1b07f9f58aa3f894f4c38a0c2/e1e28/assets/img/olab_logo.png){width=250}
+![drawing](https://d33wubrfki0l68.cloudfront.net/442bccd786f4d6c1b07f9f58aa3f894f4c38a0c2/e1e28/assets/img/olab_logo.png){width=250px}
 """
 
 st.markdown(a)
