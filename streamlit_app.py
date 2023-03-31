22fde0a48653beb9c8482ba6f8016bf2a0038bb4
smirs <saeed.mirshekari@gmail.com>
2023-03-31 17:39:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,10 @@ sns.set(rc={"figure.figsize":(22, 12)}, font_scale=5)
 import matplotlib.pyplot as plt
 import streamlit as st
 
-a = """Some text
-
-![Cool Image](src="https://d33wubrfki0l68.cloudfront.net/442bccd786f4d6c1b07f9f58aa3f894f4c38a0c2/e1e28/assets/img/olab_logo.png" style="width:200px;")
-
-Some more text"""
+a = """
+# Good Deal / Bad Deal
+<img src="https://d33wubrfki0l68.cloudfront.net/442bccd786f4d6c1b07f9f58aa3f894f4c38a0c2/e1e28/assets/img/olab_logo.png" alt="drawing" style="width:200px;"/>
+"""
 
 st.markdown(a)
 
