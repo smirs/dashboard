4acfe66ff1caa503cae5cc7fef734a3abab003e6
smirs <saeed.mirshekari@gmail.com>
2023-03-31 14:38:49 -0500

    sa

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,36 @@ from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationB
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+Address = '1976 Greenglen Dr'
+image_address = 'https://photos.zillowstatic.com/fp/466ce188280320fe0767ec885d9773a2-cc_ft_1536.png'
+#############[changes with the property]###################
+# One-Time Costs
+Buying_Cost = 169900 #450000
+
+# Initial Annual Costs [changes with the property]
+Maintenance_Annual_Cost = 12*295 #12000
+
+# Initial Annual Gains 
+Rental_Annual_Gain = 12 * (1650) #3500
+
+#############[do not change with property]#################
+# Rates 
+Interest_Rate = 0.05 #0,06
+DownPayment_Rate = 1.0 #0.20
+Appreciation_Rate = 0.04 # source: https://www.ownerly.com/real-estate/average-home-appreciation/
+Depressioation_Rate = 0.03
+Closing_Cost_Rate = 0.02
+
+DownPayment_Cost = DownPayment_Rate * Buying_Cost
+Loan_Amount = Buying_Cost - DownPayment_Cost
+Loan_Cost = Interest_Rate * Loan_Amount
+Closing_Cost = Closing_Cost_Rate * Buying_Cost
+
+# Desires
+Mortgage_Payment_Desired = 12 * 6000 #6000
+Years_ToPayOff_Desired = round((Loan_Cost + Loan_Amount)/Mortgage_Payment_Desired,2)
+
+###########################################################
 
 
 ###########################################################
