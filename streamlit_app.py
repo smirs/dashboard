7d98e3978ed5572ec96b854ddbd908a08f111163
smirs <saeed.mirshekari@gmail.com>
2023-03-31 15:19:54 -0500

    sa

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,9 +57,6 @@ ROI3 = round(Total_Gain(3) / Total_Cost(3) , 3)
 ROI5 = round(Total_Gain(5) / Total_Cost(5) , 3)
 ROI10 = round(Total_Gain(10) / Total_Cost(10) , 3)
 
-
-fig, ax = plt.subplots(figsize=(22, 12))
-
 for tick in ax.xaxis.get_major_ticks():
     tick.label.set_fontsize(20)
 
@@ -75,12 +72,15 @@ cost = list(map(Total_Cost, x))
 
 df = pd.DataFrame({'time':x, 'gain':gain, 'cost':cost})
 
-fig, ax = plt.subplots()
+ax = plt.figure(figsize=(44, 32))
+sns.lineplot(x='time', y='gain', data=df)
+st.pyplot(ax)
 
-ax = sns.lineplot(x='time', y='gain', data=df, color = 'green', label = 'gain')
+
+# sns.lineplot(x='time', y='gain', data=df, color = 'green', label = 'gain')
 # sns.lineplot(x='time', y='cost', data=df, color = 'red', label = 'cost')
 
-st.pyplot(fig)
+# st.pyplot(fig)
 
 ###########################################################
 # import streamlit as st
@@ -104,3 +104,49 @@ st.pyplot(fig)
 # plt.plot([10000, 100000, 150000, 200000, price]) 
 
 
+# import streamlit as st 
+# plt.xkcd()
+# indict = {'St. Louis, MO':'63368', 
+#           'Charlotte, NC':'28277', 
+#           'Miami, FL':'33178', 
+#           'San Francisco, CA': '95111', 
+#           'Brownsville, TX':'78520',
+#           'New York City, NY': '10001'
+#           }
+# n = NOAA()
+# observations = {}
+# for i in indict:
+#     observations[i] = pd.DataFrame(list(n.get_observations(indict[i], 'US')))
+    
+# def get_value(x):
+#     return x['value']
+# for i in indict:
+#     observations[i]['temperature_value'] = observations[i]['temperature'].apply(lambda x: get_value(x))
+#     observations[i]['temperature_value'] = observations[i]['temperature_value'] * 9/5 + 32
+#     observations[i]['timestamp'] = pd.to_datetime(observations[i]['timestamp'], infer_datetime_format=True)
+#     observations[i]['timestamp'] = pd.DatetimeIndex(observations[i]['timestamp']).tz_convert('US/Central')
+    
+# d = str(observations['St. Louis, MO']['timestamp'][0])
+# today = d[0:10]
+# ax = plt.figure(figsize=(44, 32))
+# ax.suptitle('Team Tempreture in the USA @' + today, fontsize=128)
+# plt.xlabel(' ', fontsize=24)
+# plt.ylabel(' ', fontsize=24)
+# # plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+# timewin = 10
+# for i in indict:
+#     sns.lineplot(x='timestamp', y='temperature_value', data=observations[i][observations[i]['timestamp'] >= (observations[i]['timestamp'][0] - pd.DateOffset(hours=timewin))], label= i, legend=None, linewidth = 12)
+#     plt.annotate(i, xy=(observations[i]['timestamp'][0], observations[i]['temperature_value'][0]), fontsize=48)
+    
+# xlabels = reversed(['Now','2 Hours Ago','4  Hours Ago','6  Hours Ago','8  Hours Ago','10 Hours Ago', '12 Hours Ago','14 Hours Ago','16 Hours Ago','18 Hours Ago','20 Hours Ago'])
+# xlabels = [x for x in xlabels]
+# locs_x, labels_x=plt.xticks()
+# locs_y, labels_y=plt.yticks()
+# x_ticks = []
+# plt.xticks(locs_x, xlabels, rotation=45, horizontalalignment='right', fontsize=48)
+# plt.yticks(locs_y, labels_y, rotation=0, horizontalalignment='right', fontsize=48)
+# st.pyplot(ax)
+
+# ax = plt.figure(figsize=(44, 32))
+# sns.lineplot(x='timestamp', y='temperature_value', data=df)
+# st.pyplot(ax)
