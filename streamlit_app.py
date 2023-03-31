86a767940501afd0eabea36aae283cb6dd227ccc
smirs <saeed.mirshekari@gmail.com>
2023-03-31 17:07:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,5 +119,8 @@ fig.set_xlabel('Year', fontdict={'size': 100})
 fig.set_ylabel('USD', fontdict={'size': 100})
 
 
-st.write('ROI 10 = '+ ROI10, unsafe_allow_html=False)
+st.write('ROI 1  = ', ROI1)
+st.write('ROI 5  = ', ROI5)
+st.write('ROI 10 = ', ROI10)
+
 # st.pyplot(ax)
