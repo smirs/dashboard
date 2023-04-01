6371867a0cfbe1945ca0b247b027d194f2b46fb0
smirs <saeed.mirshekari@gmail.com>
2023-03-31 21:51:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,19 +134,19 @@ fig = sns.scatterplot(x=[1, 2, 3], y=[1, 3, 2])
 fig.set_xlim(1, 100)
 fig.set_ylim(0, 1)
 
-plt.axvline(thresh_ok, 0 ,1)
+# plt.axvline(thresh_ok, 0 ,1)
 
-fig.fill_between(x, 0, thresh_ok, where= y<thresh_ok,
-                color='red', alpha=0.5, transform=ax.get_xaxis_transform())
+# fig.fill_between(x, 0, thresh_ok, where= y<thresh_ok,
+#                 color='red', alpha=0.5, transform=ax.get_xaxis_transform())
 
-plt.axvline(thresh_great, 0 ,1)
+# plt.axvline(thresh_great, 0 ,1)
 
-fig.fill_between(x, 0, thresh_great, where= (y>thresh_ok) & (y<thresh_great),
-                color='yellow', alpha=0.5, transform=ax.get_xaxis_transform())
+# fig.fill_between(x, 0, thresh_great, where= (y>thresh_ok) & (y<thresh_great),
+#                 color='yellow', alpha=0.5, transform=ax.get_xaxis_transform())
 
-fig.fill_between(x, 0, thresh_great, where= y>thresh_great,
-                color='green', alpha=0.5, transform=ax.get_xaxis_transform())
+# fig.fill_between(x, 0, thresh_great, where= y>thresh_great,
+#                 color='green', alpha=0.5, transform=ax.get_xaxis_transform())
 
-plt.axvline(thresh, 0 ,1, color='black', linestyle='--', linewidth=2)
+# plt.axvline(thresh, 0 ,1, color='black', linestyle='--', linewidth=2)
 
 st.pyplot(ax)
